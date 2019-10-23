a01074b83f76820600c861a97f890da7faade74e
Cody <chorst@uw.edu>
2019-10-23 15:04:45 -0700

    adjust logging, add raw gbd call for remission

--- src/vivarium_csu_ltbi/artifact.py
+++ src/vivarium_csu_ltbi/artifact.py
@@ -2,6 +2,7 @@ from loguru import logger
 
 import gbd_mapping
 import vivarium_inputs
+import vivarium_gbd_access.gbd as gbd
 
 """
 Outstanding issues to discuss with yongquan et al at the next meeting:
@@ -19,35 +20,34 @@ def build_cache():
     # Prevalence
     for loc in LOCATIONS:
         for id in [300, 934, 946, 947, 948, 949, 950, 954]:
+            logger.info(f"Pulling prevalence data for id={id} for {loc}")
             entity = [c for c in gbd_mapping.causes if c.gbd_id == id][0]
-            logger.info(f"Pulling prevalence data for id={id}, {entity.name} "
-                        f"for {loc}")
+            logger.info(f"Entity found for id={id}: {entity.name}")
             vivarium_inputs.get_measure(entity, 'prevalence', loc)
 
     # Cause-specific mortaltity rate
     for loc in LOCATIONS:
         for id in [300, 934, 946, 947, 948, 949, 950]:
+            logger.info(f"Pulling CSMR data for id={id} for {loc}")
             entity = [c for c in gbd_mapping.causes if c.gbd_id == id][0]
-            logger.info(f"Pulling CSMR data for id={id}, {entity.name} "
-                        f"for {loc}")
+            logger.info(f"Entity found for id={id}: {entity.name}")
             vivarium_inputs.get_measure(entity, 'cause_specific_mortality', loc)
 
     # Incidence
     # NOTE: ID 954 fails for all countries -- no non-zero data!
     for loc in LOCATIONS:
         for id in [300, 934, 946, 947, 948, 949, 950]:
+            logger.info(f"Pulling incidence data for id={id} for {loc}")
             entity = [c for c in gbd_mapping.causes if c.gbd_id == id][0]
-            logger.info(f"Pulling incidence data for id={id}, {entity.name} "
-                        f"for {loc}")
+            logger.info(f"Entity found for id={id}: {entity.name}")
             vivarium_inputs.get_measure(entity, 'incidence', loc)
 
     # Remission
+    location_ids = gbd.get_location_ids()
     for loc in LOCATIONS:
-        for id in [9422]:  # Note this is a dismod id.
-            entity = [c for c in gbd_mapping.causes if c.gbd_id == id][0]
-            logger.info(f"Pulling remission data for id={id}, {entity.name} "
-                        f"for {loc}")
-            vivarium_inputs.get_measure(entity, 'remission', loc)
+        loc_id = location_ids.loc[location_ids.location_name == loc, 'location_id']
+        logger.info(f"Pulling special remission data for dismod id=9422 for {loc}")
+        gbd.get_modelable_entity_draws(9422, loc_id)
 
 
 if __name__ == "__main__":
