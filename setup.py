5f88860b23e1cdaeba82cbd7940fa0441da71a2c
Cody Horst <chorst@uw.edu>
2019-11-18 16:10:53 -0800

    bump VPH version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     install_requirements = [
         'vivarium==0.8.24',
-        'vivarium_public_health==0.9.18',
+        'vivarium_public_health==0.9.19',
         'vivarium_cluster_tools==1.0.14',
         'vivarium_inputs[data]==3.0.1',
         
