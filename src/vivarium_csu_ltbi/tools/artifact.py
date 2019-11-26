38769dad20e9b69db4cc72443210af44a7d325f3
Cody Horst <chorst@uw.edu>
2019-11-25 18:34:17 -0800

    update csmr measure name in cache builder

--- src/vivarium_csu_ltbi/tools/artifact.py
+++ src/vivarium_csu_ltbi/tools/artifact.py
@@ -33,7 +33,7 @@ def build_cache():
         logger.info(f"Entity found for id={id}: {entity.name}")
         for loc in LOCATIONS:
             logger.info(f"Pulling for {loc}")
-            vivarium_inputs.get_measure(entity, 'cause_specific_mortality', loc)
+            vivarium_inputs.get_measure(entity, 'cause_specific_mortality_rate', loc)
 
     # Incidence
     # NOTE: ID 954 fails for all countries -- no non-zero data!
