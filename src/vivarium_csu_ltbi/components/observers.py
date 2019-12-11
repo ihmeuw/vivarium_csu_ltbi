a9426564856f760c7a0b12657d6bf29da0208b91
James Collins <collijk@uw.edu>
2019-12-10 19:43:26 -0800

    Stratify disease observer by treatment group

--- src/vivarium_csu_ltbi/components/observers.py
+++ src/vivarium_csu_ltbi/components/observers.py
@@ -37,6 +37,7 @@ class HouseholdTuberculosisDiseaseObserver(DiseaseObserver):
         self.population_view = builder.population.get_view(columns_required)
 
         self.household_tb_exposure = builder.value.get_value(f'{ltbi_globals.HOUSEHOLD_TUBERCULOSIS}.exposure')
+        self.treatment_group = builder.value.get_value('ltbi_treatment.exposure')
 
     def initialize_previous_state(self, pop_data):
         self.population_view.update(pd.Series('', index=pop_data.index, name=self.previous_state_column))
@@ -70,32 +71,34 @@ class HouseholdTuberculosisDiseaseObserver(DiseaseObserver):
     def on_time_step_prepare(self, event):
         pop = self.population_view.get(event.index)
         pop_exposure_category = self.household_tb_exposure(event.index)
+        pop_treatment_group = self.treatment_group(event.index)
 
-        for category in ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_CATEGORIES:
-            exposure_state = ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_MAP[category]
-            pop_in_category = pop.loc[pop_exposure_category == category]
+        groups = zip(ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_CATEGORIES, ltbi_globals.TREATMENT_GROUPS)
+        for exposure_category, treatment_group in groups:
+            exposure_state = ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_MAP[exposure_category]
+            pop_in_group = pop.loc[(pop_exposure_category == exposure_category)
+                                   & (pop_treatment_group == treatment_group)]
             for state in self.states:
-                state_person_time_this_step = self.get_state_person_time(pop_in_category, self.config.to_dict(),
+                state_person_time_this_step = self.get_state_person_time(pop_in_group, self.config.to_dict(),
                                                                          self.disease, state, self.clock().year,
                                                                          event.step_size, self.age_bins)
-                state_person_time_this_step = {f'{k}_{exposure_state}': v for k, v in state_person_time_this_step.items()}
+                state_person_time_this_step = {f'{k}_{exposure_state}_treatment_group_{treatment_group}': v
+                                               for k, v in state_person_time_this_step.items()}
                 self.person_time.update(state_person_time_this_step)
 
-        if self.should_sample(event.time):
-            for category in ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_CATEGORIES:
-                exposure_state = ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_MAP[category]
-                pop_in_category = pop.loc[pop_exposure_category == category]
-
-                point_population = self.get_population(pop_in_category, self.config.to_dict(),
+            if self.should_sample(event.time):
+                point_population = self.get_population(pop_in_group, self.config.to_dict(),
                                                        event.time, self.age_bins)
-                point_population = {f'{k}_{exposure_state}': v for k, v in point_population.items()}
+                point_population = {f'{k}_{exposure_state}_treatment_group_{treatment_group}': v
+                                    for k, v in point_population.items()}
                 self.total_population.update(point_population)
 
                 for state in self.states:
-                    state_point_prevalence = self.get_state_prevalent_cases(pop_in_category, self.config.to_dict(),
+                    state_point_prevalence = self.get_state_prevalent_cases(pop_in_group, self.config.to_dict(),
                                                                             self.disease, state, event.time,
                                                                             self.age_bins)
-                    state_point_prevalence = {f'{k}_{exposure_state}': v for k, v in state_point_prevalence.items()}
+                    state_point_prevalence = {f'{k}_{exposure_state}_treatment_group_{treatment_group}': v
+                                              for k, v in state_point_prevalence.items()}
                     self.prevalence.update(state_point_prevalence)
 
         # This enables tracking of transitions between states
@@ -106,22 +109,26 @@ class HouseholdTuberculosisDiseaseObserver(DiseaseObserver):
     def on_collect_metrics(self, event):
         pop = self.population_view.get(event.index)
         pop_exposure_category = self.household_tb_exposure(event.index)
+        pop_treatment_group = self.treatment_group(event.index)
 
-        for category in ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_CATEGORIES:
-            exposure_state = ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_MAP[category]
-            pop_in_category = pop.loc[(pop_exposure_category == category)]
+        groups = zip(ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_CATEGORIES, ltbi_globals.TREATMENT_GROUPS)
+        for exposure_category, treatment_group in groups:
+            exposure_state = ltbi_globals.HOUSEHOLD_TUBERCULOSIS_EXPOSURE_MAP[exposure_category]
+            pop_in_group = pop.loc[(pop_exposure_category == exposure_category)
+                                   & (pop_treatment_group == treatment_group)]
 
             for transition in ltbi_globals.HIV_TB_TRANSITIONS:
                 from_state, to_state = transition.split('_to_')
-                event_this_step = ((pop_in_category[f'{to_state}_event_time'] == event.time)
-                                   & (pop_in_category[self.previous_state_column] == from_state))
-                transitioned_pop = pop_in_category.loc[event_this_step]
+                event_this_step = ((pop_in_group[f'{to_state}_event_time'] == event.time)
+                                   & (pop_in_group[self.previous_state_column] == from_state))
+                transitioned_pop = pop_in_group.loc[event_this_step]
                 base_key = get_output_template(**self.config.to_dict()).substitute(measure=f'{transition}_event_count',
                                                                                    year=event.time.year)
                 base_filter = QueryString('')
                 transition_count = get_group_counts(transitioned_pop, base_filter, base_key,
                                                     self.config.to_dict(), self.age_bins)
-                transition_count = {f"{k}_{exposure_state}": v for k, v in transition_count.items()}
+                transition_count = {f'{k}_{exposure_state}_treatment_group_{treatment_group}': v
+                                    for k, v in transition_count.items()}
                 self.counts.update(transition_count)
 
     def metrics(self, index, metrics):
