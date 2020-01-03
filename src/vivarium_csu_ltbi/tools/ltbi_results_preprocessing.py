41411fb7fb1f722827a51e4a84dcaff8ca49d083
Yongquan Xie <yongqx2@gen-uge-submit-p01.hosts.ihme.washington.edu>
2020-01-02 16:39:14 -0800

    compile the changes for deletion of configuration of population age end

--- src/vivarium_csu_ltbi/tools/ltbi_results_preprocessing.py
+++ src/vivarium_csu_ltbi/tools/ltbi_results_preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 result_dir = '/ihme/costeffectiveness/results/vivarium_csu_ltbi/updated-input-data/'
 
-# update it as needed
+# to-do: creare two sub-values for each location, one for age_end = 10 and another for age_end = 100
 path_for_location = {
     'ethiopia': result_dir + 'ethiopia//2019_12_30_16_29_23',
     'india': result_dir + 'india/2019_12_30_16_30_19',
@@ -16,22 +16,19 @@ output_dir = '/home/j/Project/simulation_science/latent_tuberculosis_infection/r
 def load_data(country_path):
     df = pd.read_hdf(country_path + '/output.hdf')
     df = df.drop(columns='random_seed').reset_index()
-    df.rename(columns={
-        'configuration.ltbi_treatment_scale_up.scenario': 'scenario',
-        'configuration.population.age_end': 'pop_age_end'},
-        inplace=True)
+    df.rename(columns={'configuration.ltbi_treatment_scale_up.scenario': 'scenario'},
+              inplace=True)
     
-    scenario_count = (df.groupby(['input_draw', 'random_seed', 'pop_age_end'])
+    scenario_count = (df.groupby(['input_draw', 'random_seed'])
                         .scenario
                         .count())
     idx_completed = scenario_count[scenario_count == 3].reset_index()
-    idx_completed = idx_completed[['input_draw', 'random_seed', 'pop_age_end']]
+    idx_completed = idx_completed[['input_draw', 'random_seed']]
     df_completed = pd.merge(df,
                             idx_completed,
                             how='inner',
-                            on=['input_draw', 'random_seed', 'pop_age_end'])
-    
-    df_completed = df_completed.groupby(['input_draw', 'pop_age_end', 'scenario']).sum()
+                            on=['input_draw', 'random_seed'])
+    df_completed = df_completed.groupby(['input_draw', 'scenario']).sum()
     
     return df_completed
 
@@ -53,7 +50,8 @@ def get_treatment_group_from_template(template_string):
 def get_measure_from_template(template_string):
     return template_string.split('_in_')[0]
 
-def split_data(df):
+def format_data(df):
+	idx_cols = ['draw', 'scenario', 'treatment_group', 'hhtb', 'age', 'sex', 'year', 'measure']
     items = ['death', 'ylls', 'ylds', 'event_count', 'prevalent_cases', 'person_time', 'population_point_estimate']
     wanted_cols = []
     for i in df.columns:
@@ -63,7 +61,7 @@ def split_data(df):
     
     df = df[wanted_cols]
     df = (df.reset_index()
-            .melt(id_vars=['input_draw', 'pop_age_end', 'scenario'],
+            .melt(id_vars=['input_draw', 'scenario'],
                   var_name='label'))
     df['year'] = df.label.map(get_year_from_template)
     df['sex'] = df.label.map(get_sex_from_template)
@@ -72,21 +70,13 @@ def split_data(df):
     df['treatment_group'] =  df.label.map(get_treatment_group_from_template)
     df['measure'] = df.label.map(get_measure_from_template)
     df = (df.rename(columns={'input_draw': 'draw'})
-            .drop(columns='label'))
-    
-    idx_cols = ['draw', 'scenario', 'treatment_group', 'hhtb', 'age', 'sex', 'year', 'measure']
-    df_age_end_5 = (df.loc[df.pop_age_end == 5]
-                      .drop(columns='pop_age_end')
-                      .set_index(idx_cols))
-    df_age_end_100 = (df.loc[df.pop_age_end == 100]
-                        .drop(columns='pop_age_end')
-                        .set_index(idx_cols))
+            .drop(columns='label')
+            .set_index(idx_cols))
 
-    return df_age_end_5, df_age_end_100
+    return df
 
 if __name__ == '__main__':
     for location, path in path_for_location.items():
         df = load_data(path)
-        df_age_end_5, df_age_end_100 = split_data(df)
-        df_age_end_5.to_hdf(output_dir + f'{location}_indexed_age_end_5.hdf', key='data')
-        df_age_end_100.to_hdf(output_dir + f'{location}_indexed_age_end_100.hdf', key='data')
+        df = format_data(df)
+        df.to_hdf(output_dir + f'{location}_indexed.hdf', key='data')
