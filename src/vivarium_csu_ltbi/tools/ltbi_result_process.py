c7dabc107ede137d0404d00644111ba678026a09
Yaqi Wang <yaqiw@uw.edu>
2020-02-04 15:06:26 -0800

    change CI to percentile, assign 0 to NA(due to 0 denominator), merget file_100 with file_10 together

--- src/vivarium_csu_ltbi/tools/ltbi_result_process.py
+++ src/vivarium_csu_ltbi/tools/ltbi_result_process.py
@@ -1,16 +1,19 @@
 import pandas as pd, numpy as np
-import matplotlib.pyplot as plt, scipy.stats as st
+#import matplotlib.pyplot as plt, scipy.stats as st
 
 def clean_aggregate(data):
-    del data['random_seed']
-    new_data = data.reset_index().groupby(['input_draw_number', 'ltbi_treatment_scale_up.scenario']).sum()
+    del df['random_seed']
+    new_data = data.groupby(['input_draw_number', 'ltbi_treatment_scale_up.scenario']).sum()
     new_data.drop(list(new_data.filter(regex = 'person_time')), axis = 1, inplace = True)
     return new_data
 
-def readCleanDf(country, filePath):
-    df = pd.read_hdf(filePath)
-    df_100 = clean_aggregate(df)
-    return country, df_100
+def mergeCountry(df1, df2):
+    if "random_seed" in df1.columns:
+        del df1['random_seed']
+    if "random_seed" in df2.columns:
+        del df2['random_seed']
+    df = pd.merge(df1.reset_index(), df2.reset_index(), on = ["input_draw_number", "random_seed", "ltbi_treatment_scale_up.scenario"], how = 'outer')
+    return df
 
 def shape(data, pattern):
     df_data = data.filter(regex = pattern)
@@ -32,15 +35,17 @@ def addElement(data, country, measure, people, scenario,mean, lower, upper):
 
 def calculate_mean_ci(df, col_name):
     mean = df[col_name].mean()
-    lower = st.t.interval(0.95, len(df[col_name])-1, loc=np.mean(df[col_name]), scale=st.sem(df[col_name]))[0]
-    upper = st.t.interval(0.95, len(df[col_name])-1, loc=np.mean(df[col_name]), scale=st.sem(df[col_name]))[1]
+    lower = np.percentile(df[col_name], 2.5) 
+    upper = np.percentile(df[col_name], 97.5)
     return mean, lower, upper
 
 def u5_calculation(df, data, country, measure, people):
     df_u5= df[df['age_group'].isin(['early_neonatal_', 'late_neonatal_', 'post_neonatal_', '1_to_4']) & df['hh_status'].isin(['exposed'])]
     result = df_u5.groupby(['input_draw_number','ltbi_treatment_scale_up.scenario']).sum().reset_index(level = 1)
     result['3HP_averted'] = (result.loc[result['ltbi_treatment_scale_up.scenario'] == 'baseline', 'value'] - result.loc[result['ltbi_treatment_scale_up.scenario'] == '3HP_scale_up', 'value'])*100/result.loc[result['ltbi_treatment_scale_up.scenario'] == 'baseline', 'value']
+    result['3HP_averted'].fillna(0, inplace = True) # in case 0 denominator 
     result['6H_averted'] = (result.loc[result['ltbi_treatment_scale_up.scenario'] == 'baseline', 'value'] - result.loc[result['ltbi_treatment_scale_up.scenario'] == '6H_scale_up', 'value'])*100/result.loc[result['ltbi_treatment_scale_up.scenario'] == 'baseline', 'value']
+    result['6H_averted'].fillna(0, inplace = True) # in case 0 denominator
     final_df = result.reset_index()[['input_draw_number','3HP_averted','6H_averted']].drop_duplicates()
     addElement(data, country, measure, people, '6H_averted', calculate_mean_ci(final_df, '6H_averted')[0], calculate_mean_ci(final_df, '6H_averted')[1], calculate_mean_ci(final_df, '6H_averted')[2])
     addElement(data, country, measure, people, '3HP_averted', calculate_mean_ci(final_df, '3HP_averted')[0], calculate_mean_ci(final_df, '3HP_averted')[1], calculate_mean_ci(final_df, '3HP_averted')[2])
@@ -50,7 +55,9 @@ def hiv_calculation(df, data, country,  measure, people):
     df_hiv= df[df['hiv_status'].isin(['positive'])]
     result = df_hiv.groupby(['input_draw_number','ltbi_treatment_scale_up.scenario']).sum().reset_index(level = 1)
     result['3HP_averted'] = (result.loc[result['ltbi_treatment_scale_up.scenario'] == 'baseline', 'value'] - result.loc[result['ltbi_treatment_scale_up.scenario'] == '3HP_scale_up', 'value'])*100/result.loc[result['ltbi_treatment_scale_up.scenario'] == 'baseline', 'value']
+    result['3HP_averted'].fillna(0, inplace = True) # in case 0 denominator 
     result['6H_averted'] = (result.loc[result['ltbi_treatment_scale_up.scenario'] == 'baseline', 'value'] - result.loc[result['ltbi_treatment_scale_up.scenario'] == '6H_scale_up', 'value'])*100/result.loc[result['ltbi_treatment_scale_up.scenario'] == 'baseline', 'value']
+    result['6H_averted'].fillna(0, inplace = True) # in case 0 denominator
     final_df = result.reset_index()[['input_draw_number','3HP_averted','6H_averted']].drop_duplicates()
     addElement(data, country, measure, people, '6H_averted', calculate_mean_ci(final_df, '6H_averted')[0], calculate_mean_ci(final_df, '6H_averted')[1], calculate_mean_ci(final_df, '6H_averted')[2])
     addElement(data, country, measure, people, '3HP_averted', calculate_mean_ci(final_df, '3HP_averted')[0], calculate_mean_ci(final_df, '3HP_averted')[1], calculate_mean_ci(final_df, '3HP_averted')[2])
@@ -65,19 +72,37 @@ def process_data(df_total, country, measure, pattern):
 
 if __name__ == '__main__':
     result_dir = '/ihme/costeffectiveness/results/vivarium_csu_ltbi/'
-    loc_filePath = {
-        'ethiopia_end_100': result_dir + 'updated-input-data-end-100/ethiopia/2020_01_03_14_22_51/output.hdf',
-        'india_end_100': result_dir + 'updated-input-data-end-100/india/2020_01_03_14_23_42/output.hdf',
-        'peru_end_100': result_dir + 'updated-input-data-end-100/peru/2020_01_03_14_23_33/output.hdf',
-        'philippines_end_100': result_dir + 'updated-input-data-end-100/philippines/2020_01_03_14_23_28/output.hdf',
-        'south_africa_end_100': result_dir + 'updated-input-data-end-100/south_africa/2020_01_03_14_23_36/output.hdf'
+    loc_100_filePath = {
+    'ethiopia_end_100': result_dir + 'updated-input-data-end-100/ethiopia/2020_01_03_14_22_51/output.hdf',
+    'india_end_100': result_dir + 'updated-input-data-end-100/india/2020_01_03_14_23_42/output.hdf',
+    'peru_end_100': result_dir + 'updated-input-data-end-100/peru/2020_01_03_14_23_33/output.hdf',
+    'philippines_end_100': result_dir + 'updated-input-data-end-100/philippines/2020_01_03_14_23_28/output.hdf',
+    'south_africa_end_100': result_dir + 'updated-input-data-end-100/south_africa/2020_01_03_14_23_36/output.hdf'
+    }
+    loc_10_filePath = {
+    'ethiopia_end_10': result_dir + 'updated-input-data-end-10/ethiopia/2020_01_02_17_00_21/output.hdf',
+    'india_end_10': result_dir + 'updated-input-data-end-10/india/2020_01_02_17_00_23/output.hdf',
+    'peru_end_10': result_dir + 'updated-input-data-end-10/peru/2020_01_02_17_01_17/output.hdf',
+    'philippines_end_10': result_dir + 'updated-input-data-end-10/philippines/2020_01_02_17_01_17/output.hdf',
+    'south_africa_end_10': result_dir + 'updated-input-data-end-10/south_africa/2020_01_02_17_01_26/output.hdf'
     }
     measure_pattern = {"incidence": "^ltbi.*activetb", "death": "death_due_to_activetb",
                        "DALYs": "ylds_due_to_activetb|ylls_due_to_activetb"}
     data = {"country": [], "measure": [], "people": [], "scenario": [], "mean": [], "lower": [], "upper": []}
-    for location, filePath in loc_filePath.items():
-        locDf = readCleanDf(location, filePath)
+    loc_df = {}
+    for location_100, filePath_100 in loc_100_filePath.items():
+        for location_10, filePath_10 in loc_10_filePath.items():  
+            if location_100.split("_")[0] == location_10.split("_")[0]:
+                location = location_100.split("_")[0]
+                df_10 = pd.read_hdf(filePath_10)
+                df_100 = pd.read_hdf(filePath_100)
+                df_merged = mergeCountry(df_100, df_10)
+                loc_df[location] = df_merged 
+			
+    for country, df in loc_df.items():
+        locDf = clean_aggregate(df)
         for measure, pattern in measure_pattern.items():
-            process_data(locDf[1], locDf[0], measure, pattern)
+            process_data(locDf, country, measure, pattern)
 
-    pd.DataFrame(data).to_csv('/home/j/Project/simulation_science/latent_tuberculosis_infection/result/averted_result.csv')
+    #pd.DataFrame(data).to_csv('/home/j/Project/simulation_science/latent_tuberculosis_infection/result/averted_result.csv')
+    pd.DataFrame(data).to_csv('/ihme/homes/yaqiw/Yaqi_pro/averted_result_100_10_test.csv')
