1850a04cb65c341b4cd3fd770aa352513c7cd5da
Cody <chorst@uw.edu>
2020-02-12 16:38:42 -0800

    remove comments

--- src/vivarium_csu_ltbi/results_processing/table_output.py
+++ src/vivarium_csu_ltbi/results_processing/table_output.py
@@ -47,33 +47,6 @@ def scale_data(base_data, data_to_scale, join_columns, agg_columns=None, py_mult
     return scaled
 
 
-# def pivot_and_summarize(data, index_columns, prefix=''):
-#     data = (data
-#             .set_index(index_columns + ['draw'])
-#             .unstack())
-#     data.columns = data.columns.droplevel()
-#     data.columns.name = None
-#     mean = data.mean(axis=1)
-#     # CI = 95%
-#     ub = np.percentile(data, 97.5, axis=1)
-#     lb = np.percentile(data, 2.5, axis=1)
-#     data[prefix + 'mean'] = mean
-#     data[prefix + 'ub'] = ub
-#     data[prefix + 'lb'] = lb
-#     data = data[[c for c in data.columns if isinstance(c, str)]]
-#     return data
-
-#
-# def aggregate_over_treatment_group(data):
-#     groupby_cols = [c for c in data.columns if c not in ['treatment_group', 'value']]
-#     data = (data
-#             .groupby(groupby_cols)
-#             .value.sum()
-#             .reset_index())
-#     data['treatment_group'] = 'all'
-#     return data
-
-
 def make_coverage_table(mdata: MeasureData, location: str):
     pt = mdata.person_time.drop(columns='cause')
     pt['location'] = location
