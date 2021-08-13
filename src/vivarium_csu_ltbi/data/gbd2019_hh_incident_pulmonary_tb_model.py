a80264a7e4bcdd8746f2e967d1d7769123924ebc
Yongquan Xie <yongqx2@gen-uge-submit-p01.hosts.ihme.washington.edu>
2021-08-13 16:22:13 -0700

    use prepared household and tb incidence data

--- src/vivarium_csu_ltbi/data/gbd2019_hh_incident_pulmonary_tb_model.py
+++ src/vivarium_csu_ltbi/data/gbd2019_hh_incident_pulmonary_tb_model.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-from get_draws.api import get_draws
 from vivarium.interpolation import Interpolation
 
-master_dir = '/home/j/Project/simulation_science/latent_tuberculosis_infection/literature/household_structure/microdata/'
+input_dir = '/home/j/Project/simulation_science/latent_tuberculosis_infection/hh_model_input/'
 country_dict = {
     6: 'China',
     11: 'Indonesia',
@@ -26,78 +25,6 @@ country_dict = {
     198: 'Zimbabwe',
     214: 'Nigeria'
 }
-age_group_ids = [1, 23, 149, 150, 151, 152, 153, 154]
-age_group_names = ['0_to_5', '5_to_15'] + [f'{c}_to_{c+10}' for c in range(15, 65, 10)] + ['65_to_125'] 
-age_dict = dict(zip(age_group_ids, age_group_names))
-sex_dict = {1: 'Male', 2: 'Female'}
-
-def load_hh_data(location_id: int):
-    country_name = country_dict[location_id]
-    df = pd.read_stata(master_dir + country_name + '.dta').dropna()
-    if 'hhid' in df.columns:
-        df.rename(columns={'hhid': 'hh_id'}, inplace=True)
-    if country_name in ['China', 'Russian_Federation', 'South_Africa']:
-        df['age'] = df['age'].replace({'Less than 1 year': '0',
-                                       'less than 1 year': '0',
-                                       '1 year': '1',
-                                       '2 years': '2',
-                                       '100+': '100'})
-        df = df[df.age != "not reported/missing"]
-    else:
-        df['hh_id'] = df['hh_id'].str.split().map(lambda x: int(''.join(x)))
-        if country_name != 'Congo':
-            df['age'] = df['age'].replace({'95+': 95, '96+': 95, '97+': 95})
-            df = df[~((df.age == "don't know") | (df.age == "dk") | (df.age == "Don't know"))]
-    df['sex'] = df['sex'].str.capitalize()
-    df['age'] = df['age'].astype(float)
-    return df
-
-def pull_actb_incidence(location_id: int):
-    incidence_actb = get_draws(
-        gbd_id_type='cause_id',
-        source='como',
-        gbd_id=[934, 946, 947, 948, 949, 950],
-        location_id=location_id,
-        age_group_id=age_group_ids,
-        sex_id=[1,2],
-        year_id=2019,
-        measure_id=6, #incidence
-        metric_id=3, #ratio relative to population
-        gbd_round_id=6,
-        decomp_step='step5',
-    )
-    # get proportion of all-form TB incident cases
-    incidence_actb_agg = (incidence_actb
-                          .groupby(['location_id', 'age_group_id', 'sex_id', 'year_id', 'measure_id', 'metric_id'])
-                          .sum()
-                          .reset_index())
-    incidence_actb_agg.drop(columns=['measure_id', 'metric_id', 'cause_id'], inplace=True)
-    return incidence_actb_agg
-
-def pull_extra_pulmonary_tb_frac(location_id: int):
-    frac = pd.read_csv(f'/ihme/covariates/ubcov/model/output/56726/draws_temp_0/{location_id}.csv')
-    frac = frac[frac.year_id == 2019].reset_index(drop=True)
-    frac['age_group_id'] = frac.age_group_id.map({5:1,  6:23,  8:149, 10:150, 12:151, 14:152, 16:153, 18:154})
-    return frac
-
-def format_data(data: pd.DataFrame):
-    data = data.copy()
-    data['location'] = data.location_id.map(country_dict)
-    data['age_group_name'] = data.age_group_id.map(age_dict)
-    data['sex'] = data.sex_id.map(sex_dict)
-    data['year'] = 2019
-    data['age_group_start'] = data.age_group_name.map(lambda x: int(x.split('_to_')[0]))
-    data['age_group_end'] = data.age_group_name.map(lambda x: int(x.split('_to_')[1]))
-    data.drop(columns=['location_id', 'age_group_id', 'age_group_name', 'sex_id', 'year_id'], inplace=True)
-    data_long = data.melt(id_vars=['location', 'age_group_start', 'age_group_end', 'sex', 'year'], var_name=['draw'])
-    data_long['draw'] = data_long.draw.map(lambda x: int(x.split('_')[1]))
-    return data_long
-
-def get_pulmonary_tb_incidence(incidence_actb: pd.DataFrame, frac: pd.DataFrame):
-    incidence = format_data(incidence_actb).set_index(['location', 'age_group_start', 'age_group_end', 'sex', 'year', 'draw']) 
-    prop = format_data(frac).set_index(['location', 'age_group_start', 'age_group_end', 'sex', 'year', 'draw'])
-    incidence_pulmonary_tb = incidence * (1 - prop)
-    return incidence_pulmonary_tb.reset_index()
 
 def interpolation(incidence_pulmonary_tb: pd.DataFrame, hh_data: pd.DataFrame, draw: int):
     """assign the probability of incident active pulmonary TB for each household member"""
@@ -122,8 +49,8 @@ def calc_age_sex_specific_no_pulmonary_tb_prop(df: pd.DataFrame):
     for certain age and sex group
     calculate the probability of no incident active pulmonary TB case in the household
     """
-    age_bins = [0, 5] + list(range(15, 70, 10)) + [125]
     result = pd.DataFrame()
+    age_bins = [0, 5] + list(range(15, 70, 10)) + [125]
     for i, age_group_start in enumerate(age_bins[:-1]):
         age_group_end = age_bins[i+1]
         for sex in ['Male', 'Female']:
@@ -137,26 +64,24 @@ def calc_age_sex_specific_no_pulmonary_tb_prop(df: pd.DataFrame):
             result = pd.concat([result, age_sex_result], ignore_index=True)
     return result
 
-def get_estimates(draw: int):
-    """calculate the proportion of household incident active pulmonary TB contact"""
+def get_draw_estimates(file_path: str, draw: int):
     output = pd.DataFrame()
     cols = ['age_group_start', 'age_group_end', 'sex']
+    hh_data = pd.read_csv(file_path + 'hh_data.csv')
+    incidence_data = pd.read_csv(file_path + 'incidence_data.csv')
     for location_id in country_dict.keys():
-        df_hh = load_hh_data(location_id)
+        df_hh = hh_data.loc[hh_data.location_id == location_id]
         hh_ids = df_hh.hh_id.unique()
         # boostrap HH data by resampling hh_id with replacement
         sample_hhids = np.random.choice(hh_ids, size=min(len(hh_ids), 50000), replace=True)
-        df_hh = df_hh.set_index("hh_id")
-        df_hh_sample = df_hh.loc[sample_hhids].reset_index()
-        incidence_actb = pull_actb_incidence(location_id)
-        frac = pull_extra_pulmonary_tb_frac(location_id) 
-        incidence_pulmonary_tb = get_pulmonary_tb_incidence(incidence_actb, frac)
-        data = interpolation(incidence_pulmonary_tb, df_hh_sample, draw)
-        result = calc_age_sex_specific_no_pulmonary_tb_prop(data)
-        incidence_pulmonary_tb_draw = incidence_pulmonary_tb.query(f'draw == {draw}')
-        incidence_pulmonary_tb_draw['value'] = 1 - incidence_pulmonary_tb_draw['value']
+        df_hh_sample = df_hh.set_index("hh_id").loc[sample_hhids].reset_index()
+        df_incidence = incidence_data.loc[incidence_data.location_id == location_id]
+        interp = interpolation(df_incidence, df_hh_sample, draw)
+        result = calc_age_sex_specific_no_pulmonary_tb_prop(interp)
+        df_incidence_draw = df_incidence.query(f'draw == {draw}')
+        df_incidence_draw['value'] = 1 - df_incidence_draw['value']
         # pr_hhc = 1 - pr_no_pulmonary_tb_in_hh / pr_susceptible_to_pulmonary_tb
-        f = (1 - result.set_index(cols).value / incidence_pulmonary_tb_draw.set_index(cols).value).reset_index()
+        f = (1 - result.set_index(cols).value / df_incidence_draw.set_index(cols).value).reset_index()
         f['location'] = country_dict[location_id]
         output = pd.concat([output, f], ignore_index=True)
     output['year_start'] = 2019
@@ -172,6 +97,5 @@ if __name__ == '__main__':
         draw = int(os.environ['SGE_TASK_ID']) - 1
     except (KeyError, ValueError):
         draw = int(sys.argv[1])
-
-    output = get_estimates(draw)
+    output = get_draw_estimates(input_dir, draw)
     output.to_hdf(f'/share/scratch/users/yongqx2/hh_incident_pulmonary_tb_estimates/draw_{draw}.hdf', 'draw')
